f3bc8603625089f267dc2306c175092df877f49d
Bliod-Cook <108209631+Bliod-Cook@users.noreply.github.com>
2024-10-14 03:03:57 -0700

    Release 0.0.2 (#2)
    * init: actions (#1)
    
    * Release 0.0.1
    
    * fix: workflow
    
    * feat(settings): settings page init method change
    
    max and min in settings page now init using the config
    
    * fix: wrong version number

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,13 +2,20 @@
 import WinButton from "@/app/components/winButton";
 import {invoke} from "@tauri-apps/api/core";
 import { message } from "@tauri-apps/plugin-dialog";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import { Window } from "@tauri-apps/api/window";
 
 export default function Settings() {
 
     const [min, setMin] = useState(1)
     const [max, setMax] = useState(48)
+    const [hidden, setHidden] = useState(true)
+
+    useEffect(() => {
+        get_config("MIN", 1).then((result) => {setMin(result)})
+        get_config("MAX", 48).then((result) => {setMax(result)})
+        setHidden(false)
+    }, []);
 
     async function saveSettings() {
         if (max !== null && min !== null && max !== undefined && min !== undefined) {
@@ -26,12 +33,12 @@ export default function Settings() {
                 <div className={"mt-10"}>
                     <p>最小</p>
                     <input className={"border border-black text-center"}
-                           onChange={(e) => setMin(Number(e.target.value))} value={min}></input>
+                           onChange={(e) => setMin(Number(e.target.value))} defaultValue={min} hidden={hidden}></input>
                 </div>
                 <div className={"mt-1"}>
                     <p>最大</p>
                     <input className={"border border-black text-center"}
-                           onChange={(e) => setMax(Number(e.target.value))} value={max}></input>
+                           onChange={(e) => setMax(Number(e.target.value))} defaultValue={max} hidden={hidden}></input>
                 </div>
                 <div className={"mt-10"}>
                     <WinButton width={30} height={7} onClick={saveSettings}>保存</WinButton>
@@ -44,4 +51,9 @@ export default function Settings() {
 async function close_page() {
     const window = await Window.getByLabel("SettingsPage")
     window?.destroy()
+}
+
+async function get_config(config: string, default_value: number): Promise<number> {
+    const value: string = await invoke("get_config", {config: config});
+    return value === "null" ? default_value : parseInt(value.slice(1,-1))
 }
